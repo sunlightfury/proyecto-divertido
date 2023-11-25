8a4099da46e4b1a76978ac1d2b5894ae3a8b1167
pelloKraken628 <lightfury.gpsh@gmail.com>
2023-11-25 23:49:56 +0800

    third commit for multiplying 3 and 5

--- app.py
+++ app.py
@@ -1,20 +1,29 @@
 # Python program to print
 # Hello Python
 
-"""
-This would be a multiline comment in Python that 
-spans several lines and describes geeksforgeeks. 
-A Computer Science portal for geeks. It contains  
-well written, well thought  
-and well-explained computer science  
-and programming articles,  
-quizzes and more
-"""
+# a = 3
+# b = 2
 
-site = 'gtg'
+# c = a + b
+# print("sum:", c)
 
-if site == 'gfg':
-    print('Logging on to geeksforgeeks...')
-else:
-    print('retype the URL.')
-print('All set !')
+# for i in range(3, 101, 3):
+#     print("multiples of 3:", i)
+
+# for j in range(5, 101, 5):
+#     print("multiplees of 5:", j)
+
+
+# Set the range of numbers
+start_range = 3
+end_range = 100
+
+for i in range(start_range, end_range):
+
+    # Check for multiples of 3
+    if i % 3 == 0 or i % 5 == 0:
+        print("multiples of 3 and 5", i)
+
+    # # Check for multiples of 5
+    # if i % 5 == 0:
+    #     print("multiples of 5", i)
